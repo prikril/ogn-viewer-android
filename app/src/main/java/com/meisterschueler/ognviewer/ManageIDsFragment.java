04ca885523954c342554178d57241ddf06835dce
Konstantin Gründger <kammermark@gmx.de>
2016-04-12 21:30:49 +0200

    Add menu to ManageIDsFragment

--- app/src/main/java/com/meisterschueler/ognviewer/ManageIDsFragment.java
+++ app/src/main/java/com/meisterschueler/ognviewer/ManageIDsFragment.java
@@ -2,12 +2,42 @@ package com.meisterschueler.ognviewer;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
 import java.util.Map;
 
 public class ManageIDsFragment extends ListFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.manageid_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_add_item:
+                AircraftDialog.showDialog(getActivity(), "");
+
+                CustomAircraftDescriptorAdapter adapter = (CustomAircraftDescriptorAdapter) getListAdapter();
+                adapter.notifyDataSetChanged();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
