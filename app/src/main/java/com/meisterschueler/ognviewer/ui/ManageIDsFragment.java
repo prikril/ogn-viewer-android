8a95ade519f2da2a8ccd154a3cf7dd8a5c30a873
Dominik Prikril <dominik-p@gmx.de>
2018-08-20 14:28:13 +0200

    rush import export experiments

--- app/src/main/java/com/meisterschueler/ognviewer/ui/ManageIDsFragment.java
+++ app/src/main/java/com/meisterschueler/ognviewer/ui/ManageIDsFragment.java
@@ -15,9 +15,18 @@ import com.meisterschueler.ognviewer.CustomAircraftDescriptorAdapter;
 import com.meisterschueler.ognviewer.CustomAircraftDescriptorProvider;
 import com.meisterschueler.ognviewer.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import co.uk.rushorm.core.Rush;
+import co.uk.rushorm.core.RushConflict;
+import co.uk.rushorm.core.RushCore;
+
 public class ManageIDsFragment extends ListFragment {
+    private Map<String, CustomAircraftDescriptor> aircraftDescriptorMap;
+    private String jsonStringTemp;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +50,14 @@ public class ManageIDsFragment extends ListFragment {
                 adapter.notifyDataSetChanged();
 
                 break;
+
+            case R.id.action_export_list:
+                exportList();
+                break;
+
+            case R.id.action_import_list:
+                importList();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -51,7 +68,7 @@ public class ManageIDsFragment extends ListFragment {
         super.onActivityCreated(savedInstanceState);
 
         CustomAircraftDescriptorProvider adp1 = (CustomAircraftDescriptorProvider) AircraftDescriptorProviderHelper.getCustomDbAircraftDescriptorProvider();
-        Map<String, CustomAircraftDescriptor> aircraftDescriptorMap = adp1.getAircraftDescriptorMap();
+        this.aircraftDescriptorMap = adp1.getAircraftDescriptorMap();
 
         CustomAircraftDescriptorAdapter adapter = new CustomAircraftDescriptorAdapter(getActivity(), aircraftDescriptorMap.values().toArray(new CustomAircraftDescriptor[aircraftDescriptorMap.size()]));
         setListAdapter(adapter);
@@ -61,5 +78,16 @@ public class ManageIDsFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         CustomAircraftDescriptor cad = (CustomAircraftDescriptor) getListAdapter().getItem(position);
         AircraftDialog.showDialog(getActivity(), cad);
+    }
+
+    private void exportList() {
+        String jsonString = RushCore.getInstance().serialize(new ArrayList<>(this.aircraftDescriptorMap.values()));
+        this.jsonStringTemp = jsonString;
+    }
+
+    private void importList() {
+        List<Rush> objects = RushCore.getInstance().deserialize(this.jsonStringTemp);
+        List<RushConflict> conflicts = RushCore.getInstance().saveOnlyWithoutConflict(objects);
+
     }
 }
